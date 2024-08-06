d199ab26fb2acf1baf52df7e2acde298c1565872
Rex <s46129@yahoo.com.tw>
2024-08-06 19:59:13 +0800

    chore : Set IP address to 127.0.0.1

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const WebSocket = require('ws');
 
 // 定義一個函數來獲取本機的 IPv4 地址
 function getLocalIPAddress() {
-  const interfaces = os.networkInterfaces();
-  for (const name of Object.keys(interfaces)) {
-    for (const iface of interfaces[name]) {
-      // 忽略 IPv6 和內部地址（127.0.0.1）
-      if (iface.family === 'IPv4' && !iface.internal) {
-        return iface.address;
-      }
-    }
-  }
+  // const interfaces = os.networkInterfaces();
+  // for (const name of Object.keys(interfaces)) {
+  //   for (const iface of interfaces[name]) {
+  //     // 忽略 IPv6 和內部地址（127.0.0.1）
+  //     if (iface.family === 'IPv4' && !iface.internal) {
+  //       return iface.address;
+  //     }
+  //   }
+  // }
   return '127.0.0.1'; // 如果沒有找到非內部的 IPv4，返回localhost
 }
 
@@ -35,7 +35,7 @@ wss.on('connection', function connection(ws) {
     });
   });
 
-  ws.send('Welcome! You are connected to the WebSocket server.');
+  ws.send('Welcome! You are connected to the WebSocket server.4');
 });
 
 console.log(`WebSocket server is running on ws://${serverIP}:8080`);

c4cc3f109d744422f771982b65a338dee361c029
Rex <s46129@yahoo.com.tw>
2024-08-09 17:17:47 +0800

    feat : Send group player count on player join.

--- server.js
+++ server.js
@@ -3,30 +3,32 @@ const WebSocket = require('ws');
 
 // 定義一個函數來獲取本機的 IPv4 地址
 function getLocalIPAddress() {
-  // const interfaces = os.networkInterfaces();
-  // for (const name of Object.keys(interfaces)) {
-  //   for (const iface of interfaces[name]) {
-  //     // 忽略 IPv6 和內部地址（127.0.0.1）
-  //     if (iface.family === 'IPv4' && !iface.internal) {
-  //       return iface.address;
-  //     }
-  //   }
-  // }
-  return '127.0.0.1'; // 如果沒有找到非內部的 IPv4，返回localhost
+    // const interfaces = os.networkInterfaces();
+    // for (const name of Object.keys(interfaces)) {
+    //   for (const iface of interfaces[name]) {
+    //     // 忽略 IPv6 和內部地址（127.0.0.1）
+    //     if (iface.family === 'IPv4' && !iface.internal) {
+    //       return iface.address;
+    //     }
+    //   }
+    // }
+    return '127.0.0.1'; // 如果沒有找到非內部的 IPv4，返回localhost
 }
 
 const serverIP = getLocalIPAddress();
 
 // 創建 WebSocket 伺服器，綁定到自動獲取的 IP 和端口上
-const wss = new WebSocket.Server({host: serverIP, port: 8080});
+const wss = new WebSocket.Server({ host: serverIP, port: 8080 });
 
 wss.on('connection', function connection(ws) {
     console.log('A new client connected');
     wss.clients.forEach(function each(client) {
         if (client.readyState === WebSocket.OPEN) {
-            if (client === ws)
+            if (client === ws) {
                 client.send('Connect To Server Successfully');
-            else if (client !== ws)
+                client.send('PlayerCount:'+wss.clients.size);
+
+            } else if (client !== ws)
                 client.send('OnNewPlayerJoin');
         }
     });
